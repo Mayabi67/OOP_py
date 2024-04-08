11909095f7e17d5be9f102f54db1559d3f1f3fe3
Mayabi67 <160654772+Mayabi67@users.noreply.github.com>
2024-04-08 09:52:50 +0300

    changed output

--- OOP.py
+++ OOP.py
@@ -4,7 +4,7 @@ class Person:
         self.age = age
         self.gender = gender
     def intro (self):
-        print('This persons name is',self.name,'they are', self.age,'years old','and they are', self.gender,'.')
+        print('The persons name is',self.name,'they are', self.age,'years old','and they are', self.gender,'.')
 # Creating an instance of the Person class
 p1=Person('Mathias',31,'Male')
 # Calling the intro method
